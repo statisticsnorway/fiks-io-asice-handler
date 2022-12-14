fb35ecf712b8adef9405656b97b6e070b25902e1
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-12-14 23:50:49 +0100

    fiks-io-asice-handler-10

--- src/main/java/no/ks/fiks/io/asice/read/EncryptedAsicReaderImpl.java
+++ src/main/java/no/ks/fiks/io/asice/read/EncryptedAsicReaderImpl.java
@@ -116,10 +116,7 @@ public class EncryptedAsicReaderImpl implements EncryptedAsicReader {
         checkNotNull(zipOutputStream);
         checkNotNull(privatNokkel);
 
-        try (InputStream inputStream = decryptionStreamService.decrypterStream(encryptedAsic, privatNokkel)) {
-            decryptElementer(encryptedAsic, zipOutputStream, inputStream);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        InputStream inputStream = decryptionStreamService.decrypterStream(encryptedAsic, privatNokkel);
+        decryptElementer(encryptedAsic, zipOutputStream, inputStream);
     }
 }
